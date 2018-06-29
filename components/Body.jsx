006261c11fe1408d4b933d38623ff6ec4b0b6a66
Adrian Messado <adrian.messado@gmail.com>
2018-06-29 16:25:46 -0700

    add images to beer list, add style to fix image height

--- components/Body.jsx
+++ components/Body.jsx
@@ -2,8 +2,20 @@ import React from 'react'
 
 function Body() {
   return (
-    <div>
-      <p>Body</p>
+    <div className="bodyContainer">
+      <style jsx>{`
+        .bodyContainer{
+          background: url(https://images.pexels.com/photos/791810/pexels-photo-791810.jpeg);
+            background-size: 120vw;
+            background-repeat: no-repeat;
+            background-position: center top;
+            height: 300px;
+            width: 100%;
+            margin: 0;
+        }
+
+
+      `}</style>
     </div>
   )
 }
