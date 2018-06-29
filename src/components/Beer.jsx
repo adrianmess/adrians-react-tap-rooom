e8a7269e18eb5cd01f1f0cf95f5854d15201a272
Adrian Messado <adrian.messado@gmail.com>
2018-06-29 12:07:18 -0700

    add import proptypes and fill out beerlist properties in beer component

--- src/components/Beer.jsx
+++ src/components/Beer.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import PropTypes from 'prop-types';
 
-function Beer(){
+function Beer(props){
   return(
-    <div>
+    <div className="beerContainer">
+      <h3 className="beerName">{props.name}props.name</h3>
+        <p className="beerBrewer">{props.brewer}props.brewer</p>
+        <p className="beerDesc">{props.brewer}props.brewer</p>
+        <p className="beerAbv">{props.abv}props.abv</p>
+        <p className="beerPrice">{props.price}props.price</p>
+        <p className="beerRemaining">{props.remaining}props.remaining</p>
     </div>
   );
 }
