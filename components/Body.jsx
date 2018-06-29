f22cabdd93238e7b1748935fbed19bda222718ac
Adrian Messado <adrian.messado@gmail.com>
2018-06-29 16:36:53 -0700

    modify home page image style

--- components/Body.jsx
+++ components/Body.jsx
@@ -4,17 +4,18 @@ function Body() {
   return (
     <div className="bodyContainer">
       <style jsx>{`
+
+        div {
+          min-width:320px;
+        }
         .bodyContainer{
           background: url(https://images.pexels.com/photos/791810/pexels-photo-791810.jpeg);
-            background-size: 120vw;
             background-repeat: no-repeat;
+            background-size: contain;
             background-position: center top;
-            height: 300px;
-            width: 100%;
+            height: 100vh;
             margin: 0;
         }
-
-
       `}</style>
     </div>
   )
