6409af752844981748c532d355b557ddab8f559b
Adrian Messado <adrian.messado@gmail.com>
2018-06-29 11:31:04 -0700

    modify background of header

--- components/Header.jsx
+++ components/Header.jsx
@@ -11,13 +11,13 @@ function Header(){
           justify-content: space-between;
           padding: 0 50px 0 50px;
           font-family: 'Montserrat', sans-serif;
-          background: rgba(0,0,0,.25);
+          background: rgba(255,255,255,.25);
           height: 72px;
           align-items: center;
         }
         .link{
           text-decoration: none;
-          color: white;
+          color: black;
           font-size: 1.5em;
         }
       `}</style>
