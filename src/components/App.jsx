ca883ba7e9ffe2b232eeb0692d5742c95729904b
Adrian Messado <adrian.messado@gmail.com>
2018-06-29 11:20:39 -0700

    add body component to App.jsx, make body the default path

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
-import Header from './Header';
-import { Switch, Route } from 'react-router-dom';
+import React from 'react'
+import Header from './Header'
+import About from './About'
+import Body from './Body'
+
+import { Switch, Route } from 'react-router-dom'
 
 function App(){
   return (
     <div>
       <Header/>
       <Switch>
+        <Route exact path='/' component={Body} />
         <Route exact path='/about' component={About} />
-
       </Switch>
     </div>
   );
