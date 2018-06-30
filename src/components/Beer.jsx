c94370cf32838c8626220b7af2a51ed88df8cd07
Adrian Messado <adrian.messado@gmail.com>
2018-06-29 17:14:16 -0700

    add styling and grid to Beer list

--- src/components/Beer.jsx
+++ src/components/Beer.jsx
@@ -3,25 +3,39 @@ import PropTypes from 'prop-types';
 
 function Beer(props){
   return (
-    <div>
+    <div classNames="grid">
       <style>{`
-          img{
-            height:200px;
-          }
 
+        .grid{
+                    display: grid;
+                    grid-template-columns: 1fr 4fr;
+                    margin: 5vw 5vw;
+                  }
+                  img{
+                    height: 150px;
+                    margin-right: 50px;
+                  }
+                  images {
+                    max-width: 400px;
+                  }
+  
+
+                  .beerInfo{
+                    max-height: 390px;
+                    max-width: 420px;
+                  }
             `}
-
       </style>
       <div className="images">
           <img src={props.img}/>
       </div>
-      <div>
+      <div className="beerInfo">
         <h3>{props.name}</h3>
         <p>{props.brewer}</p>
         <p>{props.description}</p>
         <p>{props.abv}</p>
         <p>{props.price}</p>
-        <hr/>
+
       </div>
     </div>
 
