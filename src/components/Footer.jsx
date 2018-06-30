c67f847ccc7f3ec8e47d83ca80743e0efbbccf21
Adrian Messado <adrian.messado@gmail.com>
2018-06-30 10:17:01 -0700

    add compyright info to bottom, align text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,13 +5,25 @@ function Footer(){
     <div className="footer">
       <style>{`
           .footer {
-            background-color: gray;
+            background-color: black;
             height: 100px;
             bottom:0;
-            width: 98%;
+            width: 99%;
+            margin: auto;
           }
+          .copyright {
+            color: white;
+            text-align: center;
+            // margin-top: 25%;
+            padding-top: 4%;
+          }
+
           `}
         </style>
+
+          <p className="copyright">Copyright @ Adrians Tap Room 2018</p>
+
+
       </div>
     )
   }
