be5d70e9344a6f835741d5081910edff55dc5f4b
Adrian Messado <adrian.messado@gmail.com>
2018-06-29 18:02:14 -0700

    add css style to footer, fix to bottom of page

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 function Footer(){
   return (
-    <div><p>Footer</p></div>
+    <div className="footer">
+      <style>{`
+          .footer {
+            background-color: gray;
+            height: 100px;
+            position: absolute;
+            width: 100%;
+            bottom: 0;
+          }
+            `}
+      </style>
+      <p>Footer</p>
+    </div>
   )
 }
 
