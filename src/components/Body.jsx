ce3698ace9e473c75d3ecceeb7477f90925861b7
Adrian Messado <adrian.messado@gmail.com>
2018-06-29 18:50:13 -0700

    add body button images, add flex to align images

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -17,22 +17,42 @@ function Body() {
             margin: 0;
           }
           .bodyContainer {
-                        margin-bottom: 300px;
+            margin-bottom: 300px;
+          }
+          .secondBodyButtons{
+            display:flex;
+          }
+          .bodyButtonImg{
+            height: 150px;
+            border-radius: 25% 25%;
+            align-self: center;
+          }
+          .bodyButtonText{
+            text-align: center;
+          }
+          .bodyButtonsContainer{
+            display: flex;
+            flex-direction: column;
+            width: 300px;
           }
           `}</style>
         <div className="bodyTopImage"></div>
 
         <div className="secondBodyButtons">
-          <div className="events">
-            <h3>Events</h3>
-            <img src={require('../assets/images/drinks/hamms.jpg')} />
+          <div className="events bodyButtonsContainer">
+            <h3 className="bodyButtonText">Events</h3>
+            <img src={require('../assets/images/bodyButtons/adult-asia-bar-small.jpg')} className="bodyButtonImg" />
+
           </div>
 
-          <div className="whatOnTap">
-            <h3>What's On tap</h3>
+          <div className="whatOnTap bodyButtonsContainer">
+            <h3 className="bodyButtonText">What's On tap</h3>
+            <img src={require('../assets/images/bodyButtons/beer-tap-small2.jpg')} className="bodyButtonImg" />
+
           </div>
-          <div className="location">
-            <h3>location</h3>
+          <div className="location bodyButtonsContainer">
+            <h3 className="bodyButtonText">Menu</h3>
+            <img src={require('../assets/images/bodyButtons/menu-crop-small.jpg')} className="bodyButtonImg" />
           </div>
         </div>
       </div>
