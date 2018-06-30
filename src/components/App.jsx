9f0d6bfe315c8a5121d6dedb9b83703d572ba135
Adrian Messado <adrian.messado@gmail.com>
2018-06-29 17:03:14 -0700

    add footer to App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,16 +2,25 @@ import React from 'react';
 import Header from './Header';
 import BeerList from './BeerList';
 import Body from './Body';
+import Footer from './Footer'
 import { Switch, Route } from 'react-router-dom';
 
 function App(){
   return (
-    <div>
+    <div className="bodyContainer">
+      <style> {`
+          .bodyContainer {
+            min-width: 320px;
+          }
+            `}
+
+      </style>
       <Header/>
       <Switch>
         <Route exact path='/' component={Body} />
         <Route exact path='/beer' component={BeerList} />
       </Switch>
+      <Footer/>
     </div>
   );
 }
