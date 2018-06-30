fe137b9dffa6f0d55b33be12e02b30a0ccad3aef
Adrian Messado <adrian.messado@gmail.com>
2018-06-29 19:45:57 -0700

    add image for about section, add parallax effect

--- components/Body.jsx
+++ components/Body.jsx
@@ -21,6 +21,7 @@ function Body() {
           }
           .secondBodyButtons{
             display:flex;
+            margin-top: 5vh;
           }
           .bodyButtonImg{
             height: 150px;
@@ -35,6 +36,20 @@ function Body() {
             flex-direction: column;
             width: 300px;
           }
+
+          .thirdBody{
+            margin-top: 10vh;
+            height: 30vh;
+            color: black;
+            background: url(https://images.pexels.com/photos/128242/pexels-photo-128242.jpeg);
+            background-attachment: fixed !important;
+            background-repeat: no-repeat;
+            background-size: cover !important;
+            text-align: center;
+            overflow: hidden;
+            // padding: 170px 0 184px 0;
+          }
+
           `}</style>
         <div className="bodyTopImage"></div>
 
@@ -54,6 +69,10 @@ function Body() {
             <h3 className="bodyButtonText">Menu</h3>
             <img src={require('../assets/images/bodyButtons/menu-crop-small.jpg')} className="bodyButtonImg" />
           </div>
+        </div>
+
+        <div className="thirdBody parallax-section">
+
         </div>
       </div>
 
