379ce302018479f1413cff8ee1a0cc43672e840d
Adrian Messado <adrian.messado@gmail.com>
2018-06-29 19:09:11 -0700

    align list to center, adjust styling

--- src/components/Beer.jsx
+++ src/components/Beer.jsx
@@ -3,50 +3,50 @@ import PropTypes from 'prop-types';
 
 function Beer(props){
   return (
-    <div classNames="grid">
+    <div className="grid">
       <style>{`
 
-        .grid{
-                    display: grid;
-                    grid-template-columns: 1fr 4fr;
-                    margin: 5vw 5vw;
-                  }
-                  img{
-                    height: 150px;
-                    margin-right: 50px;
-                  }
-                  images {
-                    max-width: 400px;
-                  }
+          .grid{
+            display: flex;
+            margin-left: 20%;
+            margin-right: 20%;
+            margin-top: 5%;
+          }
 
-                  .beerInfo{
-                    max-height: 390px;
-                    max-width: 420px;
-                  }
-            `}
-      </style>
-      <div className="images">
+          img{
+            height: 150px;
+            margin-right: 50px;
+          }
+          .images {
+            width: 200px;
+          }
+
+          .beerInfo{
+            max-height: 390px;
+            max-width: 420px;
+          }
+          `}
+        </style>
+        <div className="images">
           <img src={props.img}/>
+        </div>
+        <div className="beerInfo">
+          <h3>{props.name}</h3>
+          <p>{props.brewer}</p>
+          <p>{props.description}</p>
+          <p>{props.abv}</p>
+          <p>{props.price}</p>
+        </div>
       </div>
-      <div className="beerInfo">
-        <h3>{props.name}</h3>
-        <p>{props.brewer}</p>
-        <p>{props.description}</p>
-        <p>{props.abv}</p>
-        <p>{props.price}</p>
+    );
+  }
 
-      </div>
-    </div>
+  Beer.propTypes = {
+    name: PropTypes.string.isRequired,
+    brewer: PropTypes.string,
+    description: PropTypes.string,
+    abv: PropTypes.string,
+    price: PropTypes.string
+  };
 
-  );
-}
-
-Beer.propTypes = {
-  name: PropTypes.string.isRequired,
-  brewer: PropTypes.string,
-  description: PropTypes.string,
-  abv: PropTypes.string,
-  price: PropTypes.string
-};
-
-export default Beer;
+  export default Beer;
