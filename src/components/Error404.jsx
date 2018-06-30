c736a340cfffdc50cb8b3b97c11ab3d063d1348d
Adrian Messado <adrian.messado@gmail.com>
2018-06-29 20:58:55 -0700

    add image to 404 page

--- src/components/Error404.jsx
+++ src/components/Error404.jsx
@@ -3,9 +3,19 @@ import { Link } from 'react-router-dom';
 
 function Error404(){
   return (
-    <div>
-      <h2>The page you are looking for does not exist!</h2>
-      <h3>Would you like to return <Link to="/">home</Link> instead?</h3>
+    <div className="404container">
+      <style>{`
+
+          a:visited{color:blue !important}
+            `}</style>
+      <div>
+        <img src={require('../assets/images/404image.png')}></img>
+      </div>
+      <div>
+        <h2>The page you are looking for does not exist!</h2>
+        <h3>Would you like to return <Link to="/" style={{textDecoration: 'none'}}><a>home</a></Link> instead?</h3>
+      </div>
+
     </div>
   );
 }
