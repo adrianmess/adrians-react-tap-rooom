7e3a34f15982b4fc2e20d09e55a7b18e49261e75
Adrian Messado <adrian.messado@gmail.com>
2018-07-06 09:13:18 -0700

    create/modify newbeerform to include all needed fields for creating new beer

--- src/components/NewTicketForm.jsx
+++ src/components/NewTicketForm.jsx
@@ -1,24 +1,33 @@
 import React from 'react';
 
-function NewTicketForm(){
+function NewBeerForm(){
   return (
     <div>
       <form>
         <input
           type='text'
           id='names'
-          placeholder='Pair Names'/>
+          placeholder='Beer Name'/>
         <input
           type='text'
-          id='location'
-          placeholder='Location'/>
+          id='brewer'
+          placeholder='Brewer Name'/>
         <textarea
-          id='issue'
-          placeholder='Describe your issue.'/>
-        <button type='submit'>Help!</button>
+          type='text'
+          id='description'
+          placeholder='Beer Description'/>
+        <textarea
+          type='text'
+          id='abv'
+          placeholder='Alcohol by volume'/>
+        <textarea
+          type='text'
+          id='price'
+          placeholder='Price'/>
+        <button type='submit'>Add</button>
       </form>
     </div>
   );
 }
 
-export default NewTicketForm;
+export default NewBeerForm;
