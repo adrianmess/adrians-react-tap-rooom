4acb1f213c6eb9622919b625fcced3d3a36a99c3
Adrian Messado <adrian.messado@gmail.com>
2018-07-06 09:27:19 -0700

    create confirmation question component

---
+++ src/components/ConfirmationQuestion.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ConfirmationQuestion(){
+  return (
+    <div>
+      <p>Are you sure you want to add this new beer to the list?</p>
+      <button>Yes</button>
+    </div>
+  );
+}
+
+export default ConfirmationQuestion;
