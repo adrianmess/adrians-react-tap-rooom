153686bf028c445d2c62c7ed8ab3a9be857d8a89
Adrian Messado <adrian.messado@gmail.com>
2018-07-06 11:54:47 -0700

    finish defining newbeer prop type

--- src/components/NewBeerControl.jsx
+++ src/components/NewBeerControl.jsx
@@ -18,19 +18,22 @@ class NewBeerControl extends React.Component {
   }
 
   render(){
-      let currentlyVisibleContent = null;
-      if (this.state.formVisibleOnPage){
-        currentlyVisibleContent = <NewBeerForm onNewBeerCreation={this.props.onNewBeerCreation}/>;
-      } else {
-        currentlyVisibleContent = <ConfirmationQuestion onAddBeerConfirmation={this.handleAddBeerConfirmation}/>;
-      }
-      return (
-        <div>
-          {currentlyVisibleContent}
-        </div>
-      );
+    let currentlyVisibleContent = null;
+    if (this.state.formVisibleOnPage){
+      currentlyVisibleContent = <NewBeerForm onNewBeerCreation={this.props.onNewBeerCreation}/>;
+    } else {
+      currentlyVisibleContent = <ConfirmationQuestion onAddBeerConfirmation={this.handleAddBeerConfirmation}/>;
     }
-
+    return (
+      <div>
+        {currentlyVisibleContent}
+      </div>
+    );
+  }
 }
+
+NewBeerControl.propTypes = {
+  onNewTicketCreation: PropTypes.func
+};
 
 export default NewBeerControl;
