0f1dae66e74448621b065288e783dd7320348bf2
Adrian Messado <adrian.messado@gmail.com>
2018-07-06 10:01:49 -0700

    add NewBeerControlComponent to app.jsx component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import Header from './Header';
 import BeerList from './BeerList';
 import Body from './Body';
 import Footer from './Footer';
+import NewBeerControl from './NewBeerControl';
 import Error404 from './Error404';
 import { Switch, Route } from 'react-router-dom';
 
@@ -20,6 +21,7 @@ function App(){
       <Switch>
         <Route exact path='/' component={Body} />
         <Route exact path='/beer' component={BeerList} />
+        <Route exact path='/addbeer' component={NewBeerControl} />
         <Route component={Error404} />
       </Switch>
       <Footer/>
